5bdde19932ebeac6f65279e8b0a4722d698a91ff
ruben-vl <email@rubenvanlaer.com>
2024-12-07 14:25:50 +0100

    Simple printing to console

---
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!");
+
+            Console.WriteLine(3 + 3);
+
+            Console.Write("Hello World! ");
+            Console.WriteLine("I will print on the same line.");
+        }
+    }
+}
