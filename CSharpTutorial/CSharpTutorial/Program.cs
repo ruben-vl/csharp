5d726aeae9d727bb981623b8f449dac6a1168a1b
ruben-vl <email@rubenvanlaer.com>
2024-12-07 16:25:26 +0100

    Linq

--- CSharpTutorial/CSharpTutorial/Program.cs
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
+using System.Linq;
 
-namespace HelloWorld
+namespace MyApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-            cars[0] = "Opel";
-            Console.WriteLine(cars[0]);
-            Console.WriteLine(cars.Length);
-
-            string[] temp;
-            temp = new string[] {"Volvo", "BMW", "Ford"};
+            int[] myNumbers = {5, 1, 8, 9};
+            Console.WriteLine(myNumbers.Max());
+            Console.WriteLine(myNumbers.Min());
+            Console.WriteLine(myNumbers.Sum());
         }
     }
 }
