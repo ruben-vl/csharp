078efe40184efe1b8b28dbb52b6a2e5f033e9052
ruben-vl <email@rubenvanlaer.com>
2024-12-07 16:30:37 +0100

    Multidimensional arrays

--- CSharpTutorial/CSharpTutorial/Program.cs
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -7,10 +7,22 @@ namespace MyApplication
     {
         static void Main(string[] args)
         {
-            int[] myNumbers = {5, 1, 8, 9};
-            Console.WriteLine(myNumbers.Max());
-            Console.WriteLine(myNumbers.Min());
-            Console.WriteLine(myNumbers.Sum());
+            int[,] numbers = {{1,4,2},{3,6,8}};
+            numbers[0,2] = 5;
+            // Console.WriteLine(numbers[0,2]);
+
+            // foreach (int i in numbers)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.WriteLine(numbers[i,j]);
+                }
+            }
         }
     }
 }
