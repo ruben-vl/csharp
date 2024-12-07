d77d7247cbc9359c16e9a50551840c6063089ca4
ruben-vl <email@rubenvanlaer.com>
2024-12-07 16:34:24 +0100

    Methods

--- CSharpTutorial/CSharpTutorial/Program.cs
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Linq;
 
-namespace MyApplication
+namespace CSharpTutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[,] numbers = {{1,4,2},{3,6,8}};
-            numbers[0,2] = 5;
-            // Console.WriteLine(numbers[0,2]);
+            MyMethod();
+            MyMethod();
+            MyMethod();
+        }
 
-            // foreach (int i in numbers)
-            // {
-            //     Console.WriteLine(i);
-            // }
-
-            for (int i = 0; i < numbers.GetLength(0); i++)
-            {
-                for (int j = 0; j < numbers.GetLength(1); j++)
-                {
-                    Console.WriteLine(numbers[i,j]);
-                }
-            }
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed!");
         }
     }
 }
