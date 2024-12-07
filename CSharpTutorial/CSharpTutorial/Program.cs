d3549764db2d3163f57f33bba1ddd5f1fda88f13
ruben-vl <email@rubenvanlaer.com>
2024-12-07 15:15:24 +0100

    String interpolation

--- CSharpTutorial/CSharpTutorial/Program.cs
+++ CSharpTutorial/CSharpTutorial/Program.cs
@@ -6,19 +6,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string greeting = "Hello";
-            string greeting2 = "Nice to meet you!";
-
-            // string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            // Console.WriteLine("The length of the txt string is: " + txt.Length);
-
-            string txt = "Hello World";
-            Console.WriteLine(txt.ToUpper());   // Outputs "HELLO WORLD"
-            Console.WriteLine(txt.ToLower());   // Outputs "hello world"
-
-            string firstName = "John ";
+            string firstName = "John";
             string lastName = "Doe";
-            string name = firstName + lastName;
+            string name = $"My full name is: {firstName} {lastName}";
             Console.WriteLine(name);
         }
     }
